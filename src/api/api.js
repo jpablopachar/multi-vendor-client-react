c4d453f997cb31b7fea52c8681621cd7a8630a9d
jpablopachar <jpablopachar1993@gmail.com>
2024-09-27 08:13:02 -0500

    Corrección para utilizar las variables de entorno

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: `${import.meta.VITE_API_URL}/api` })
+const api = axios.create({ baseURL: `${import.meta.env.VITE_API_URL}/api` })
 
 export default api
